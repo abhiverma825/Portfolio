3f55dfe0398b5748acdf8ca76f17b92210bbad77
abhiverma825 <abhiverma825@gmail.com>
2025-12-26 21:10:38 +0530

    third commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,12 +14,12 @@ void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
-
+   // /Users/abhishekverma/Downloads/flutter_setup
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
+    // Tap the '+' icon and trigger a q1frame.
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
